1fca97d457e5eec38bd97eb7e8851250ac5b7986
Maximilian Stiefel <stiefel.maximilian@online.de>
2017-09-19 18:01:59 +0200

    Fixed a small mistake. Astar is executed now every turn.

--- Assignment 1/code/ourFunction.R
+++ Assignment 1/code/ourFunction.R
@@ -17,6 +17,7 @@ library(DeliveryMan)
 # Source files
 #############################################################################################
 source('aStar.R')
+source('hFunctions.R')
 
 #############################################################################################
 # Global variables
@@ -33,16 +34,14 @@ ourFunction <- function(traffic, car, packages){
 	if( !exists("state", where=car$mem) )
 	{
 		car$mem$state <- "EMPTY"
-		car$mem$moves <- c()
+		car$mem$goal <- c()
 		car$mem$packageCarried <- NA
-		car$mem$vroads <- list()
-		car$mem$hroads <- list()
 	}
 
 	# Main state machine
-	car$nextMove <- NA
 	repeat
 	{
+		car$nextMove <- NA
 		if( car$mem$state == "EMPTY" )
 		{
 			# Do calculations here where to pick up the next parcel
@@ -61,38 +60,39 @@ ourFunction <- function(traffic, car, packages){
 					}
 				}
 			}
-			#Settle goal
+			# Settle goal
 			car$mem$packageCarried <- lowest_goal[1]
-			goal <- packages[lowest_goal[1], c(1,2)]
-			cat("Pick up parcel at\n")
+			car$mem$goal <- packages[lowest_goal[1], c(1,2)]
+			cat("You are at\n")
 			print(start)
-			print(goal)
-			car$mem$moves <- astar_res$moves
+			cat("Pick up parcel at\n")
+			print(car$mem$goal)
 			car$mem$state <- "MOVING_EMPTY"	
 		}
 		else if( car$mem$state == "MOVING_EMPTY" )	
 		{
-			# Move the car until there are no more movements 
-			car$nextMove <- car$mem$moves[1]
-			car$mem$moves <- car$mem$moves[-1]
-			if( length(car$mem$moves) == 0 )
+			# Move the car until goal reached
+			if( xy2Index(c(car$x, car$y)) == xy2Index(car$mem$goal) )	
 				car$mem$state <- "LOADED"
+			else
+				car$nextMove <- AStar(c(car$x, car$y), car$mem$goal, traffic$hroads, traffic$vroads)$moves[1]	
 		}
-		else if( car$mem$ state == "LOADED" )	
+		else if( car$mem$state == "LOADED" )	
 		{
-			# Do calculations where to deliver parcel
+			# Specify where parcel should go
 			start <- c(car$x, car$y)
-			goal <- packages[car$mem$packageCarried, c(3,4)]
-			car$mem$moves <- AStar(start, goal, traffic$hroads, traffic$vroads)$moves 
+			car$mem$goal <- packages[car$mem$packageCarried, c(3,4)]
+			cat("Deliver to\n")
+			print(car$mem$goal)
 			car$mem$state <- "MOVING_LOADED"	
 		}
-		else if( car$mem$ state == "MOVING_LOADED" )	
+		else if( car$mem$state == "MOVING_LOADED" )	
 		{
 			# Move the car until there are no more movements
-			car$nextMove <- car$mem$moves[1]
-                        car$mem$moves <- car$mem$moves[-1]
-                        if( length(car$mem$moves) == 0 )
-				car$mem$state <- "EMPTY"	
+			if( xy2Index(c(car$x, car$y)) == xy2Index(car$mem$goal) )
+				car$mem$state <- "EMPTY"
+			else	
+				car$nextMove <- AStar(c(car$x, car$y), car$mem$goal, traffic$hroads, traffic$vroads)$moves[1] 
 		}
 	
 		cat("State: ")
@@ -110,4 +110,4 @@ ourFunction <- function(traffic, car, packages){
 }
 
 # Run delivery man game
-runDeliveryMan(carReady = ourFunction, dim = 10, turns = 1000, doPlot = T, pause = 0.1, del = 5);
+runDeliveryMan(carReady = ourFunction, dim = 10, turns = 1000, doPlot = T, pause = 0.1, del = 3);
