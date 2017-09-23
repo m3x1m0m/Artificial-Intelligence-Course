c09dc786bcb9669204586d00e996b2951d6d452a
Maximilian Stiefel <stiefel.maximilian@online.de>
2017-09-23 18:19:11 +0200

    Final version to hand in is ready.

--- Assignment 1/code/ourFunction.R
+++ Assignment 1/code/ourFunction.R
@@ -2,7 +2,7 @@
 #
 # Project:	Assignment 1, Artificial Intelligence, Uppsala University
 # Author: 	Maximilian Stiefel
-# Last mod.:	18.09.2017
+# Last mod.:	23.09.2017
 # File:		ourFunction.R 
 # Description:	Function to interface with the A* algorithm
 #
@@ -35,7 +35,7 @@ ourFunction <- function(traffic, car, packages){
 	{
 		car$mem$state <- "EMPTY"
 		car$mem$goal <- c()
-		car$mem$packageCarried <- NA
+		car$mem$packageCarried <- NA	
 	}
 
 	# Main state machine
@@ -44,36 +44,68 @@ ourFunction <- function(traffic, car, packages){
 		car$nextMove <- NA
 		if( car$mem$state == "EMPTY" )
 		{
-			# Do calculations here where to pick up the next parcel
+			car$mem$packageCarried <- NA
+			car$mem$goal <- NA
 			start <- c(car$x, car$y)
-			# Initialize with high value
-			lowest_goal <- c(1, 1000)
+			# Check if accidentaly ended up on a pickup point.
+			# Do not determine the goal in that case.
+			# Jump directly to MOVING_LOADED.
 			for( i in 1:dim(packages)[1] )
 			{
-				if( packages[i, 5] == 0 ) 
+				if ( packages[i, 5] == 1 )
 				{
-					astar_res <- AStar(start, packages[i, c(1,2)], traffic$hroads, traffic$vroads)
-					if( astar_res$cost < lowest_goal[2] )
+					car$mem$packageCarried <- i
+			        	car$mem$goal <- packages[i, c(3,4)]	
+					# This is some stupid case. Just wait one turn.
+					if( xy2Index(start) == xy2Index(car$mem$goal) )
 					{
-						# Better goal found -> it is closer
-						lowest_goal <- c(i, astar_res$cost)
+						car$nextMove <- 5
+						car$mem$state <- "EMPTY"
+					}
+					else
+						car$mem$state <- "MOVING_LOADED"	
+					break;
+				}
+	
+			}
+			# Not accidentally ended up on a pickup up point.
+			if( is.na(car$mem$packageCarried) )
+			{
+				# Do calculations here where to pick up the next parcel.
+				# Initialize with high value.
+				lowest_goal <- c(1, 1000)
+				for( i in 1:dim(packages)[1] )
+				{
+					if( packages[i, 5] == 0 ) 
+					{
+						astar_res <- AStar(start, packages[i, c(1,2)], traffic$hroads, traffic$vroads)
+						if( astar_res$cost < lowest_goal[2] )
+						{
+							# Better goal found -> it is closer..
+							lowest_goal <- c(i, astar_res$cost)
+						}
 					}
 				}
+				# Settle goal
+				car$mem$packageCarried <- lowest_goal[1]
+				car$mem$goal <- packages[lowest_goal[1], c(1,2)]
+				# This is some stupid case. Just wait one turn.
+				if( xy2Index(start) == xy2Index(car$mem$goal) )
+				{
+					car$nextMove <- 5
+					car$mem$state <- "EMPTY"
+				}
+				else
+					car$mem$state <- "MOVING_EMPTY"	
 			}
-			# Settle goal
-			car$mem$packageCarried <- lowest_goal[1]
-			car$mem$goal <- packages[lowest_goal[1], c(1,2)]
-			cat("You are at\n")
-			print(start)
-			cat("Pick up parcel at\n")
-			print(car$mem$goal)
-			car$mem$state <- "MOVING_EMPTY"	
 		}
 		else if( car$mem$state == "MOVING_EMPTY" )	
 		{
 			# Move the car until goal reached
-			if( xy2Index(c(car$x, car$y)) == xy2Index(car$mem$goal) )	
+			if( xy2Index(c(car$x, car$y)) == xy2Index(car$mem$goal) )
+			{
 				car$mem$state <- "LOADED"
+			}
 			else
 				car$nextMove <- AStar(c(car$x, car$y), car$mem$goal, traffic$hroads, traffic$vroads)$moves[1]	
 		}
@@ -82,24 +114,18 @@ ourFunction <- function(traffic, car, packages){
 			# Specify where parcel should go
 			start <- c(car$x, car$y)
 			car$mem$goal <- packages[car$mem$packageCarried, c(3,4)]
-			cat("Deliver to\n")
-			print(car$mem$goal)
 			car$mem$state <- "MOVING_LOADED"	
 		}
 		else if( car$mem$state == "MOVING_LOADED" )	
 		{
 			# Move the car until there are no more movements
 			if( xy2Index(c(car$x, car$y)) == xy2Index(car$mem$goal) )
+			{
 				car$mem$state <- "EMPTY"
+			}
 			else	
 				car$nextMove <- AStar(c(car$x, car$y), car$mem$goal, traffic$hroads, traffic$vroads)$moves[1] 
 		}
-	
-		cat("State: ")
-		print(car$mem$state)
-		cat("Next move: ")
-		print(car$nextMove)
-		#readline(prompt="Press ENTER")
 
 		# Break only if a new next move is determined
 		# Do not lose turns
@@ -108,6 +134,3 @@ ourFunction <- function(traffic, car, packages){
 	}
 		return(car)
 }
-
-# Run delivery man game
-runDeliveryMan(carReady = ourFunction, dim = 10, turns = 1000, doPlot = T, pause = 0.1, del = 3);
