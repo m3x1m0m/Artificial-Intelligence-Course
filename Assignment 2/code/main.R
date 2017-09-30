bfa355ca7cf380180fc29ab898bc41e3160f4609
Maximilian Stiefel <stiefel.maximilian@online.de>
2017-10-01 00:13:31 +0200

    Done for today.

--- Assignment 2/code/main.R
+++ Assignment 2/code/main.R
@@ -34,10 +34,10 @@ makeTransitionM <- function(edges)
 	TM <- matrix(0, nrow=NWATERH, ncol=NWATERH)
 
 	# For every water hole
-	for(i in 1:dim(edges)[1])
+	for(i in 1:NWATERH)
 	{
-		# Check the whole matrix
-		buff <- c()
+		# Check the whole edges matrix
+		buff <- i
 		for(j in 1:dim(edges)[1])
 		{
 			# Detected a vertice to another node
@@ -47,7 +47,7 @@ makeTransitionM <- function(edges)
 				buff <- rbind(buff, edges[j,1])
 		}	
 		nvertices <- length(buff)
-		# Use gained knowledge to create transition matrix
+		# Use gained knowledge to modify transition matrix
 		for(m in buff)
 		{
 			TM[m, i] <- 1/nvertices
@@ -63,15 +63,74 @@ makeTransitionM <- function(edges)
 #############################################################################################
 makeEmissionM <- function(readings, gauss)
 {
-	EM <- matrix(0, nrow=40, ncol=3)
+	EM <- matrix(0, nrow=NWATERH, ncol=NOBSS)
+	# For every waterhole convert salinity, phosphate and nitrogen to a probability
 	for(i in 1:NWATERH)
 	{
 		for(j in 1:NOBSS)
 		{
-			EM[i,j] <- dnorm(readings[j], gauss[[j]][i,1], gauss[[j]][i,2])		
+			EM[i,j] <- dnorm(readings[j], gauss[[j]][i,1], gauss[[j]][i,2])
+			#print(gauss[[j]][i,1])
+			#print(gauss[[j]][i,2])
+			#print(EM[i,j])
+			#print(readings[j])		
 		}	
 	}
-	return(EM)
+	# The three variables are independent 
+	# So P(A,B,C) = P(A), P(B), P(C) 
+	EM <- EM[,1] * EM[,2] * EM[,3]
+	EMT <- matrix(0, nrow=NWATERH, ncol=NWATERH)
+	for(i in 1:NWATERH)
+	{
+		EMT[i,i] <- EM[i]
+	}
+	return(EMT)
+}
+
+#############################################################################################
+#
+# Function to normalize the state matrix
+#
+#############################################################################################
+normalizeStateM <- function(SM)
+{
+	sum <- 0
+	for(i in 1:NWATERH)
+	{
+		sum <- sum + SM[i]	
+	}
+	SM <- SM/sum
+	return(SM)
+}
+
+#############################################################################################
+#
+# Function to predict state that lies x steps in the future (Markov Chain)
+#
+#############################################################################################
+predictState <- function(SM, TM, x)
+{
+		return(SM %*% TM^x)
+}
+
+#############################################################################################
+#
+# Function to determine which waterhole the croc is sitting after x turns
+#
+#############################################################################################
+mostLikelyWH <- function(SM)
+{
+	highest <- c(0,0)
+	print(SM)
+	for(i in 1:NWATERH)
+	{
+		# New highest probability?
+		if(SM[i] > highest[1])
+		{
+			highest <- c(SM[i], i)
+		}	
+	}
+	return(highest)
 }
 
 #############################################################################################
@@ -81,9 +140,46 @@ makeEmissionM <- function(readings, gauss)
 #############################################################################################
 ourFunction <- function(mnm, readings, positions, edges, gauss)
 {
-	print(makeEmissionM(readings, gauss))
+	if( !exists("state", where=mnm$mem) )
+	{
+		# Initialize ...
+		# State matrix
+		mnm$mem$SM <- matrix(1/NWATERH, ncol=NWATERH, nrow=1)
+		# Transition matrix
+		mnm$mem$TM <- makeTransitionM(edges)
+		# State
+		mnm$mem$state <- "WAIT"
+		print(mnm$mem$TM)
+	}
+	else
+	{
+		# Go through all positions and check whether the croc is eating sb.
+		for(i in 1:length(positions))
+		{
+			# If TRUE sb has been eaten
+			if( is.na(positions[i]) )
+				next
+			if(positions[i] < 0)
+			{
+				SM <- matrix(0, ncol=NWATERH, nrow=1)
+				SM[-positions[i]] <- 1
+			}		
+		}
+		# Get transition matrix and status vector
+		SM <- mnm$mem$SM
+		TM <- mnm$mem$TM
+		# Generate emission matrix from readings
+		EM <- makeEmissionM(readings, gauss)
+		# Perform forward algorithm
+		SM <- SM %*% TM %*% EM 
+	       	SM <- normalizeStateM(SM)
+		mnm$me$SM <- SM
+		highest <- mostLikelyWH(SM)
+		print(highest)	
+	}
+	mnm$moves <- c(0,0)
 	readline(prompt="Well we are waiting :D")
-	#return(mnm)
+	return(mnm)
 }
 
-runWheresCroc(ourFunction, showCroc = T, pause = 0.1)
+runWheresCroc(ourFunction, showCroc = T, pause = 0)
